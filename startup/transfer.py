f00965b05bd43983c70810b21bc07b09e0220c33
Rafael Neuwirth Swierczynski <rafanswi@protonmail.com>
2023-09-28 14:23:00 -0300

    fixed bugs in transfer.py script

--- startup/transfer.py
+++ startup/transfer.py
@@ -3,16 +3,16 @@ import shutil
 
 name = '[TRANSFER SCRIPT]'
 saveconfig_line: str = '#*# <---------------------- SAVE_CONFIG ---------------------->'
-saveconfig_backup = [
-    '#*# <---------------------- SAVE_CONFIG ---------------------->',
-    '#*# DO NOT EDIT THIS BLOCK OR BELOW. The contents are auto-generated.',
-    '#*#',
-    '#*# [probe]',
-    '#*# z_offset = -0.300',
-    '#*#',
-    '#*# [stepper_z]',
-    '#*# position_endstop = 340.25',
-]
+saveconfig_backup = """
+#*# <---------------------- SAVE_CONFIG ---------------------->
+#*# DO NOT EDIT THIS BLOCK OR BELOW. The contents are auto-generated.
+#*#
+#*# [probe]
+#*# z_offset = -0.300
+#*#
+#*# [stepper_z]
+#*# position_endstop = 340.25
+"""
 
 core = os.path.join('/home', 'pi', 'SyncraftCore')
 pdc = os.path.join ('/home', 'pi', 'printer_data', 'config')
@@ -22,6 +22,9 @@ class PATH:
     PDC_FRESH = os.path.join(core, 'store', 'fresh', 'printerdataconfig')
     PDC_MACHINE = pdc
     PDC_CACHE = os.path.join(core, 'cache', 'pdc')
+    MOONRAKER = os.path.join('/home', 'pi', 'moonraker')
+    KLIPPER = os.path.join('/home', 'pi', 'klipper')
+    MAINSAIL = os.path.join('/home', 'pi', 'mainsail')
     class FILE:
         class PDC_FRESH_BACKUP:
             KS = os.path.join(core, 'store', 'fresh', 'printerdataconfig', 'backups', 'backup-KlipperScreen.conf')
@@ -45,6 +48,20 @@ class BOOL:
 
 extracted_saveconfig = []
 
+def securePermission ():
+    dirs = [PATH.CORE, PATH.PDC_MACHINE, PATH.MAINSAIL, PATH.KLIPPER]
+    for path in dirs:
+        try:
+            for dirpath, dirnames, filenames in os.walk(path):
+                shutil.chown(dirpath, 1000, 1000)
+                for filename in filenames:
+                    shutil.chown(os.path.join(dirpath, filename), 1000, 1000)
+        except Exception as e:
+            print(f"{name} ☓ User 'pi' can't own '{path}'.")
+            print(f"{name} {e}")
+
+securePermission()
+
 # DELETE ALL CONTENT ON PDC CACHE
 if os.path.exists(PATH.PDC_CACHE):
     print(f'{name} ✓ Deleting all content in PDC Cache.')
@@ -53,8 +70,9 @@ else:
     print(f"{name} ☓ PDC Cache not found. Creating it.")
     try:
         os.makedirs(PATH.PDC_CACHE)
-    except:
+    except Exception as e:
         print(f"{name} ☓ Error trying to create PDC Cache Path, maybe a permission error?")
+        print(f"{name} {e}")
         exit()
 
 # TRY TO FIND THE SAVECONFIG LINE IN PRINTER.CFG
@@ -76,6 +94,14 @@ else:
 
 # CREATE A FILE IN CACHE AND WRITE IT'S LINES BASED ON THE EXTRACTED SAVECONFIG LINE
 try:
+    if not os.path.exists(PATH.PDC_CACHE):
+        os.makedirs(PATH.PDC_CACHE)
+        print(f'{name} ✓ Created PDC Cache.')
+except Exception as e:
+    print(f'{name} ☓ Error trying to create PDC Cache.')
+    print(f"{name} {e}")
+    exit()
+try:
     with open(PATH.FILE.CACHE.PRINTER, 'w') as extracted:
         if (BOOL.SAVE_LINES):
             extracted.writelines(extracted_saveconfig)
@@ -83,8 +109,9 @@ try:
         else:
             extracted.writelines(saveconfig_backup)
             print(f'{name} ✓ Used Default Values to write the contents of SaveConfig.')
-except:
+except Exception as e:
     print(f'{name} ☓ Error trying to open/create the Printer File in PDC Cache.')
+    print(f"{name} {e}")
     exit()
 
 # COPY BOTH KLIPPERSCREEN.CONF AND VARIABLES.CFG TO PDC CACHE
@@ -126,8 +153,9 @@ else:
     print(f"{name} ☓ PDC Machine not found. This really should't happen... Creating it.")
     try:
         os.makedirs(PATH.PDC_MACHINE)
-    except:
+    except Exception as e:
         print(f"{name} ☓ Error trying to create PDC Machine Path, maybe a permission error?")
+        print(f"{name} {e}")
         exit()
 
 # TRANSFER ALL CONTENT FROM PDC_FRESH TO MACHINE
@@ -143,16 +171,18 @@ try:
     shutil.copyfile(PATH.FILE.CACHE.KS, PATH.FILE.MACHINE.KS)
     shutil.copyfile(PATH.FILE.CACHE.VARIABLES, PATH.FILE.MACHINE.VARIABLES)
     print(print(f'{name} ✓ Transferred both KS and Variables Files from PDC Cache to PDC Machine.'))
-except:
+except Exception as e:
     print(f"{name} ☓ Error trying to copy files from Cache to PDC Machine.")
+    print(f"{name} {e}")
     exit()
 
 # TRANSFORM 'BACKUP' PRINTER.CFG FILE INTO USEFUL FILE
 try:
     shutil.copyfile(PATH.FILE.PDC_BACKUP.PRINTER, PATH.FILE.MACHINE.PRINTER)
     print(print(f'{name} ✓ PDC Machine Backup Printer file transformed into normal file.'))
-except:
+except Exception as e:
     print(print(f'{name} ☓ Error trying to transform PDC Machine Backup Printer File into normal file.'))
+    print(f"{name} {e}")
     exit()
 
 # APPEND CONTENT TO THAT PRINTER.CFG FILE
@@ -163,12 +193,8 @@ try:
             for line in extracted:
                 printer_cfg.write(line)
     print(print(f'{name} ✓ Added SaveConfig content to PDC Machine Printer file.'))
-except:
+except Exception as e:
     print(print(f'{name} ☓ Error while trying to append SaveConfig content into PDC Machine Printer File'))
+    print(f"{name} {e}")
 
-# MAKE SURE THAT USER 'pi' OWNS THE MACHINE PATH
-try:
-    os.chown(PATH.PDC_MACHINE, 1000, 1000)
-    print(print(f'{name} ✓ User \'pi\' now owns PDC Machine Directory.'))
-except:
-    print(print(f'{name} ☓ User \'pi\' can\'t own PDC Machine Directory.'))
+securePermission()
