2ac4ef6323b7bf781f6f58a1ff94c9577d6373f6
Rafael Neuwirth Swierczynski <rafanswi@protonmail.com>
2023-09-29 15:48:16 -0300

    add usb update

---
+++ usb/update/apply.sh
@@ -0,0 +1,88 @@
+#################
+# KLIPPERSCREEN #
+#################
+
+cd /home/pi
+
+name="[USB UPDATE SCRIPT]"
+
+process="$name Update Syncraft X1 KlipperScreen"
+softwares_dir=/home/pi/printer_data/gcodes/USB/*/SYNCRAFT/content/KlipperScreen
+fresh_dir=/home/pi/SyncraftCore/state/fresh/KlipperScreen
+
+echo "[HELPER] START: $process."
+if [ -d "$fresh_dir" ]; then
+    sudo rm -r $fresh_dir
+fi
+sudo cp -r $softwares_dir /home/pi
+sudo chown -R pi:1000 $fresh_dir
+echo "[HELPER] DONE: $process."
+
+
+############
+# MAINSAIL #
+############
+
+cd /home/pi
+
+process="$name Update Syncraft Mainsail"
+softwares_dir=/home/pi/printer_data/gcodes/USB/*/SYNCRAFT/content/mainsail
+fresh_dir=/home/pi/SyncraftCore/state/fresh/mainsail
+
+echo "[HELPER] START: $process."
+if [ -d "$fresh_dir" ]; then
+    sudo rm -r $fresh_dir
+fi
+sudo cp -r $softwares_dir /home/pi
+sudo chown -R pi:1000 $fresh_dir
+echo "[HELPER] DONE: $process."
+
+
+#############
+# MOONRAKER #
+#############
+
+cd /home/pi
+
+process="$name Update Moonraker"
+fresh_dir=/home/pi/SyncraftCore/state/fresh/moonraker
+softwares_dir=/home/pi/printer_data/gcodes/USB/*/SYNCRAFT/content/moonraker
+install_script=/home/pi/SyncraftCore/state/fresh/moonraker/scripts/install-moonraker.sh
+
+echo "[HELPER] START: $process."
+if [ -d "$fresh_dir" ]; then
+    sudo rm -r $fresh_dir
+fi
+sudo cp -r $softwares_dir /home/pi
+
+if [ -e "$install_script" ]; then
+    sudo -u pi bash $install_script
+fi
+
+sudo chown -R pi:1000 $fresh_dir
+
+echo "[HELPER] DONE: $process."
+
+
+#####################
+# PRINTERDATACONFIG #
+#####################
+
+cd /home/pi
+
+process="$name Update PDC"
+fresh_dir=/home/pi/SyncraftCore/state/fresh/printerdataconfig
+softwares_dir=/home/pi/printer_data/gcodes/USB/*/SYNCRAFT/content/config
+
+echo "[HELPER] START: $process."
+if [ -d "$fresh_dir" ]; then
+    sudo rm -r $fresh_dir
+fi
+
+sudo cp -r $softwares_dir /home/pi/printer_data/
+
+sudo chown -R pi:1000 $fresh_dir
+
+echo "[HELPER] DONE: $process."
+
+echo "[HELPER] ALL DONE!"
