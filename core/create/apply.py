cc7a817d30c48469f8bf8a140b7f95c20f04b8b5
Rafael Neuwirth Swierczynski <rafanswi@protonmail.com>
2023-10-10 14:26:04 -0300

    update invader

--- core/create/apply.py
+++ core/create/apply.py
@@ -5,7 +5,7 @@ class SCRIPT:
     DOWNLOAD = os.path.join(core, 'core', 'update', 'apply.py')
     UPGRADE = os.path.join(core, 'state', 'upgrade', 'apply.sh')
     MACHINE = os.path.join(core, 'machine', 'apply.sh')
-    TRANSFER = os.path.join(core, 'startup', 'transfer.py')
+    TRANSFER = os.path.join(core, 'startup', '1_transfer.py')
 
 scripts = [
     SCRIPT.DOWNLOAD, SCRIPT.UPGRADE, SCRIPT.MACHINE, SCRIPT.TRANSFER
