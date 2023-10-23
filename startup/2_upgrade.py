5746be95dcd207bb2121554bb9d3fc7948718308
Rafael Neuwirth Swierczynski <rafanswi@protonmail.com>
2023-10-23 16:44:11 -0300

    updated 2_upgrade.py on 'startup'

--- startup/2_upgrade.py
+++ startup/2_upgrade.py
@@ -5,9 +5,15 @@ upgrade_all = os.path.join(state_dir, 'upgrade', 'apply.sh')
 upgrade_ks = os.path.join(state_dir, 'upgrade', 'ks', 'apply.sh')
 upgrade_mainsail = os.path.join(state_dir, 'upgrade', 'mainsail', 'apply.sh')
 
+machine_ks = os.path.join('/home', 'pi', 'KlipperScreen')
+
 # Upgrade Mainsail
 os.system(f'sudo bash {upgrade_mainsail}')
 
 # Upgrade KS
 os.system(f'sudo bash {upgrade_ks}')
+
+if os.path.exists(machine_ks):
+    os.system(f'sudo chown -R pi:1000 {machine_ks}')
+
 os.system('sudo service KlipperScreen restart')
