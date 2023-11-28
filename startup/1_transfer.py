6cfc164f45c05167740dd4b78529ae848a5b7357
Rafael Neuwirth Swierczynski <rafanswi@protonmail.com>
2023-11-28 11:55:49 -0300

    update

--- startup/1_transfer.py
+++ startup/1_transfer.py
@@ -194,7 +194,7 @@ try:
             with open (PATH.FILE.PDC_BACKUP.PRINTER, 'w') as printercfg_file:
                 config.write(printercfg_file)
                 print(print(f'{name} ✓ PDC Machine Backup Printer file now has updated Canbus UUID.'))
-except e:
+except Exception as e:
     print(print(f'{name} ☓ Error trying to update Canbus UUID in printer cfg file.'))
     print(f'error: {e}')
 
