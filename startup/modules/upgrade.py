4b4b4e8eb40f72fdb06990c02f2cbbba0ea1344c
Rafael Neuwirth Swierczynski <rafanswi@protonmail.com>
2023-12-13 09:30:23 -0300

    change machine_ks to DIR.SYSTEM.KS.PATH in upgrade.py

--- startup/modules/upgrade.py
+++ startup/modules/upgrade.py
@@ -9,7 +9,7 @@ def upgrade():
     # Upgrade KS
     os.system(f'sudo bash {DIR.STATE.UPGRADE.KS.APPLY}')
 
-    if os.path.exists(machine_ks):
+    if os.path.exists(DIR.SYSTEM.KS.PATH):
         os.system(f'sudo chown -R pi:1000 {DIR.SYSTEM.KS.PATH}')
 
     os.system('sudo service KlipperScreen restart')
