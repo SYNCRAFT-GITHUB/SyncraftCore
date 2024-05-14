13df0977f77d54e77a5029353e6036e8907a02e3
RafaelSwi <rafanswi@protonmail.com>
2024-05-14 11:55:57 -0300

    add flash/replash placeholder scripts

---
+++ fixes/reflash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# So far this script has absolutely nothing.
+
+# In this script, instructions must be given to
+# flash/reflash the controller board.
+
+# There is a panel in SwierVision "flash.py"
+# to execute this script.
+
+# This panel is currently hidden, there is no access to it normally.
+
+# When this script is complete,
+# the panel should be released for access.
