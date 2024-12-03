77da74fa3f1773d5778d85913f1d65e6a812f776
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2024-12-03 16:13:31 -0300

    Fix directory name and stash changes before checkout

--- startup/startup.sh
+++ startup/startup.sh
@@ -2,17 +2,20 @@
 
 cd /home/pi
 cd printer_data/config
-git checkout v1-homologacao
 git pull
 
 if [ $? -neq 0 ]; then
 	exit 1
 fi
 
+git stash
+# TODO: Trocar para v1
+git checkout v1-homologacao
+
 # replace rc.local with new one
 
 sudo rm -rf /etc/rc.local
-sudo cp config/build/rc.local /etc
+sudo cp build/rc.local /etc
 
 # remove SyncraftCore
 
