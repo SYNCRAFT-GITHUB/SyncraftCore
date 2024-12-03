9dc7061428590f4e8d5da1a931179fc838996462
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2024-12-03 15:37:39 -0300

    Implement kill SyncraftCore startup script

--- startup/script.py
+++ startup/script.py
@@ -1,14 +1,3 @@
-from startup.modules.transfer import transfer
-from startup.modules.upgrade import upgrade
-from startup.modules.restart_services import restart_services
-from startup.modules.intro import play_boot_video
+import os
 
-# rafaelSwi /*
-# Executes all Python functions in sequence when booting the machine.
-# If you want to add something, create the function in a separate file,
-# import the file, and call the function.
-
-transfer()
-upgrade()
-restart_services()
-play_boot_video()
+os.system("sudo sh /home/pi/SyncraftCore/startup/startup.sh")
---
+++ startup/startup.sh
@@ -0,0 +1,20 @@
+# update IDEXConfig
+
+cd /home/pi
+cd printer_data/config
+git checkout v1-homologacao
+git pull
+
+if [ $? -neq 0 ]; then
+	exit 1
+fi
+
+# replace rc.local with new one
+
+sudo rm -rf /etc/rc.local
+sudo cp config/build/rc.local /etc
+
+# remove SyncraftCore
+
+cd /home/pi
+rm -rf SyncraftCore
