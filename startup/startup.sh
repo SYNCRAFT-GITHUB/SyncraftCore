d5ef7170d59f534cf68fbd36f07c3f9c72a80607
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2024-12-04 14:11:36 -0300

    run every command as sudo

--- startup/startup.sh
+++ startup/startup.sh
@@ -2,28 +2,28 @@
 
 cd /home/pi
 cd printer_data/config
-git stash
-git pull
+sudo git stash
+sudo git pull
 
 if [ $? -neq 0 ]; then
 	exit 1
 fi
 
 # TODO: Trocar para v1
-git checkout v1-homologacao
+sudo git checkout v1-homologacao
 
 # update SwierVision
 cd /home/pi
 cd SwierVision
-git stash
-git pull
+sudo git stash
+sudo git pull
 
 if [ $? -neq 0 ]; then
 	exit 1
 fi
 
 # TODO: Trocar para idex
-git checkout idex-dev
+sudo git checkout idex-dev
 
 # replace rc.local with new one
 
@@ -35,6 +35,6 @@ sudo cp build/rc.local /etc
 # remove SyncraftCore
 
 cd /home/pi
-rm -rf SyncraftCore
+sudo rm -rf SyncraftCore
 
 sudo reboot
