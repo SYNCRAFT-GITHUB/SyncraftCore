b176b4538c98c0535394852a6d175fea424c7f15
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2024-12-04 14:40:07 -0300

    output states to file

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,40 +1,42 @@
+output_file=/home/pi/syncraftcore_kill_output.txt
+echo "" > $output_file
+
 # update IDEXConfig
 
-cd /home/pi
-cd printer_data/config
-sudo git stash >> /home/pi/idexconfig_output.txt
-sudo git pull >> /home/pi/idexconfig_output.txt
+cd /home/pi/printer_data/config
+sudo git branch >> $output_file
+sudo git stash >> $output_file
+sudo git pull >> $output_file
 
 if [ $? -neq 0 ]; then
 	exit 1
 fi
 
 # TODO: Trocar para v1
-sudo git checkout v1-homologacao >> /home/pi/idexconfig_output.txt
+sudo git checkout v1-homologacao >> $output_file
+sudo git branch >> $output_file
 
 # update SwierVision
-cd /home/pi
-cd SwierVision
-sudo git stash >> /home/pi/idexconfig_output.txt
-sudo git pull >> /home/pi/idexconfig_output.txt
+cd /home/pi/SwierVision
+sudo git stash >> $output_file
+sudo git pull >> $output_file
 
 if [ $? -neq 0 ]; then
 	exit 1
 fi
 
 # TODO: Trocar para idex
-sudo git checkout idex-dev >> /home/pi/idexconfig_output.txt
+sudo git checkout idex-dev >> $output_file
 
 # replace rc.local with new one
 
-cd /home/pi
-cd printer_data/config
-sudo rm -rf /etc/rc.local >> /home/pi/idexconfig_output.txt
-sudo cp build/rc.local /etc >> /home/pi/idexconfig_output.txt
+cd /home/pi/printer_data/config
+sudo rm -rf /etc/rc.local >> $output_file
+sudo cp build/rc.local /etc >> $output_file
 
 # remove SyncraftCore
 
 cd /home/pi
-sudo rm -rf SyncraftCore >> /home/pi/idexconfig_output.txt
+sudo rm -rf SyncraftCore >> $output_file
 
 sudo reboot
